5733dd73a1d9afb4e90e09d710eee407543c2eb6
SeongChae Na <SeongChae.Na@hermessol.com>
2025-12-10 16:23:12 +0900

    set

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -26,5 +26,10 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, '../dist/client'),
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
   },
 });
